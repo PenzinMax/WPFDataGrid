9c5e77a9528f2ba1670e5e637c701603c18a65a5
PenzinMax <99172843+PenzinMax@users.noreply.github.com>
2024-03-14 10:24:02 +0500

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.ObjectModel;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml;
+
+namespace WpfApp3
+{
+    
+    public partial class MainWindow : Window
+    {
+        public ObservableCollection<Period> period = null;
+
+        public MainWindow()
+        {
+            
+
+            period = new ObservableCollection<Period>() {
+                new Period(){
+                        DateStart = DateTime.Now,
+                        DateFinish = DateTime.Now,
+                        Type = 1
+                }, new Period(){
+                        DateStart = DateTime.Now,
+                        DateFinish = DateTime.Now,
+                        Type = 2
+                }, new Period(){
+                        DateStart = DateTime.Now,
+                        DateFinish = DateTime.Now,
+                        Type = 3
+                }
+            };
+            /*foreach (var item in period)
+            {
+                MessageBox.Show(item.DateFinish.ToString());
+            }
+            */
+        }
+    }
+}
---
+++ Period.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp3
+{
+    public class Period 
+    {
+        public DateTime DateStart { get; set; }
+        public DateTime DateFinish { get; set; }
+
+        public int Type { get; set; }
+    }
+}
